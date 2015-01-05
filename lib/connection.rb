e04851a5f500862e1a6fae43dadfe0777ce871dd
jabattack1 <peter.hahn@live.com>
2015-01-05 11:53:10 -0500

    project

--- lib/connection.rb
+++ lib/connection.rb
@@ -1,4 +1,12 @@
+# require 'active_record'
+
+
+# ActiveRecord::Base.establish_connection({:adapter => "postgresql", :host => "localhost", :username => "root", :database => "project"})
+
+
+
 require 'active_record'
 
+ActiveRecord::Base.establish_connection('postgresql://' + ENV["DB_INFO"]  + '@127.0.0.1/project')
 
-ActiveRecord::Base.establish_connection({:adapter => "postgresql", :host => "localhost", :username => "root", :database => "project"})
+ActiveRecord::Base.logger = Logger.new(STDOUT)  
